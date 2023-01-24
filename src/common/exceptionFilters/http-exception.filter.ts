f798d88298f51d6f8f3289c3ed0197083199d348
Grimdal032 <112388311+Grimdal032@users.noreply.github.com>
2023-01-24 22:12:33 +0900

    Changes to be committed: 	modified:   src/common/exceptionFilters/http-exception.filter.ts  fix #7

--- src/common/exceptionFilters/http-exception.filter.ts
+++ src/common/exceptionFilters/http-exception.filter.ts
@@ -1,14 +1,17 @@
-
-import { ExceptionFilter, Catch, ArgumentsHost, HttpException } from '@nestjs/common';
+import { ExceptionFilter, Catch, ArgumentsHost, HttpException, HttpStatus } from '@nestjs/common';
 import { Response } from 'express';
 
-@Catch()
+@Catch(HttpException, Error)
 export class HttpExceptionFilter implements ExceptionFilter {
-    catch(exception: HttpException, host: ArgumentsHost) {
+    catch(exception: HttpException | Error, host: ArgumentsHost) {
         const ctx = host.switchToHttp();
         const response = ctx.getResponse<Response>();
         const request = ctx.getRequest<Request>();
-        const status = exception.getStatus();
+        const status =
+        exception instanceof HttpException
+            ? exception.getStatus()
+            : HttpStatus.INTERNAL_SERVER_ERROR;
+
         const errorMessage = exception.message;
         console.error(exception);
         response.status(status).json({
