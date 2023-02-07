a17b35fd6f5092667057f8616d688ffc50c2cbf7
Grimdal032 <112388311+Grimdal032@users.noreply.github.com>
2023-02-07 15:43:54 +0900

    <FIX> Offset Test

--- src/goal/goal.service.ts
+++ src/goal/goal.service.ts
@@ -20,62 +20,79 @@ export class GoalService {
   async getAllGoals(take: number, page: number): Promise<[Goals[], number]> {
     return await this.goalRepository
       .createQueryBuilder('g')
-      .where('g.status IN (:...statuses)', { statuses: ["recruit", "proceeding"] })
+      .where('g.status IN (:...statuses)', {
+        statuses: ['recruit', 'proceeding'],
+      })
       .andWhere('g.headCount != 1')
       .leftJoin('g.userId', 'users')
       .select(['g', 'users.userId', 'users.nickname'])
       .orderBy('g.createdAt', 'DESC')
-      .take(take)
-      .skip(take * ( page - 1 ))
+      .limit(take)
+      .offset(take * (page - 1))
       .getManyAndCount();
   }
 
   async searchGoal(
-    keyword: string, sortOby: string, statuses: string[],
-    min: number, max: number, orderby: 'ASC'|'DESC',
-    take: number, page: number
-    ): Promise<[Goals[], number]>{
+    keyword: string,
+    sortOby: string,
+    statuses: string[],
+    min: number,
+    max: number,
+    orderby: 'ASC' | 'DESC',
+    take: number,
+    page: number,
+  ): Promise<[Goals[], number]> {
     return await this.goalRepository
       .createQueryBuilder('g')
-      .where('g.status IN (:...statuses)', {statuses})
+      .where('g.status IN (:...statuses)', { statuses })
       .andWhere(`${sortOby} BETWEEN ${min} AND ${max}`)
       .andWhere('g.headCount != 1')
-      .andWhere(new Brackets((qb) => {
-        qb.where('g.title like :keyword', { keyword: `%${keyword}%` })
-          .orWhere('g.hashTag like :keyword', { keyword: `%${keyword}%` })
-      }))
+      .andWhere(
+        new Brackets((qb) => {
+          qb.where('g.title like :keyword', {
+            keyword: `%${keyword}%`,
+          }).orWhere('g.hashTag like :keyword', { keyword: `%${keyword}%` });
+        }),
+      )
       .leftJoin('g.userId', 'users')
       .select(['g', 'users.userId', 'users.nickname'])
       .orderBy(`${sortOby}`, `${orderby}`)
       .take(take)
-      .skip(take * ( page - 1 ))
+      .skip(take * (page - 1))
       .getManyAndCount();
   }
 
   async searchGoalNotValue(
-    keyword: string, sortOby: string, statuses: string[], orderby: 'ASC'|'DESC',
-    take: number, page: number
-    ): Promise<[Goals[], number]>{
+    keyword: string,
+    sortOby: string,
+    statuses: string[],
+    orderby: 'ASC' | 'DESC',
+    take: number,
+    page: number,
+  ): Promise<[Goals[], number]> {
     return await this.goalRepository
       .createQueryBuilder('g')
-      .where('g.status IN (:...statuses)', {statuses})
+      .where('g.status IN (:...statuses)', { statuses })
       .andWhere('g.headCount != 1')
-      .andWhere(new Brackets((qb) => {
-        qb.where('g.title like :keyword', { keyword: `%${keyword}%` })
-          .orWhere('g.hashTag like :keyword', { keyword: `%${keyword}%` })
-      }))
+      .andWhere(
+        new Brackets((qb) => {
+          qb.where('g.title like :keyword', {
+            keyword: `%${keyword}%`,
+          }).orWhere('g.hashTag like :keyword', { keyword: `%${keyword}%` });
+        }),
+      )
       .leftJoin('g.userId', 'users')
       .select(['g', 'users.userId', 'users.nickname'])
       .orderBy(`${sortOby}`, `${orderby}`)
       .take(take)
-      .skip(take * ( page - 1 ))
+      .skip(take * (page - 1))
       .getManyAndCount();
   }
 
-  async getImminentGoal(take: number, status: string) : Promise<Goals[]> {
+  async getImminentGoal(take: number, status: string): Promise<Goals[]> {
     return await this.goalRepository
       .createQueryBuilder('g')
-      .where('g.status = :status', {status})
+      .where('g.status = :status', { status })
       .andWhere('g.curcount != g.headcount')
       .leftJoin('g.userId', 'users')
       .select(['g', 'users.userId', 'users.nickname'])
@@ -87,7 +104,7 @@ export class GoalService {
   async getGoalDetail(goalId: number): Promise<Goals> {
     return await this.goalRepository
       .createQueryBuilder('g')
-      .where('g.goalId = :goalId', {goalId})
+      .where('g.goalId = :goalId', { goalId })
       .leftJoin('g.userId', 'users')
       .select(['g', 'users.userId', 'users.nickname'])
       .getOne();
@@ -96,22 +113,30 @@ export class GoalService {
   async getGoalByGoalId(goalId: number): Promise<Goals> {
     return await this.goalRepository
       .createQueryBuilder('g')
-      .where('g.goalId = :goalId', {goalId})
+      .where('g.goalId = :goalId', { goalId })
       .leftJoin('g.userId', 'users')
       .select(['g', 'users.userId'])
       .getOne();
   }
 
-  async getStartGoalByStatus(status: string, aDate: string, bDate: string): Promise<Goals[]>{
+  async getStartGoalByStatus(
+    status: string,
+    aDate: string,
+    bDate: string,
+  ): Promise<Goals[]> {
     return await this.goalRepository.find({
-      where: { status, startDate: Between(new Date(aDate), new Date(bDate)) }
-    })
+      where: { status, startDate: Between(new Date(aDate), new Date(bDate)) },
+    });
   }
 
-  async getEndGoalByStatus(status: string, aDate: string, bDate: string): Promise<Goals[]>{
+  async getEndGoalByStatus(
+    status: string,
+    aDate: string,
+    bDate: string,
+  ): Promise<Goals[]> {
     return await this.goalRepository.find({
-      where: { status, endDate: Between(new Date(aDate), new Date(bDate)) }
-    })
+      where: { status, endDate: Between(new Date(aDate), new Date(bDate)) },
+    });
   }
 
   // 목표 참가자 숫자 변화
